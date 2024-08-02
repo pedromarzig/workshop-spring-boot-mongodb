9931c9d3a62be559360375c86dee4a692d99388e
pedro marzig <pedromarzigcastelani@gmail.com>
2024-08-02 09:03:56 -0300

    Consulta simples com @Query

--- src/main/java/com/pedromarzig/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pedromarzig/workshopmongo/repository/PostRepository.java
@@ -3,13 +3,18 @@ package com.pedromarzig.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pedromarzig.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> FindByTitle(String text);
+	
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
