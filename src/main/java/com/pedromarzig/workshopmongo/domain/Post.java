4a7dc6385a800c7f8b905bc23befc6dfd92266df
pedro marzig <pedromarzigcastelani@gmail.com>
2024-08-02 08:37:55 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/pedromarzig/workshopmongo/domain/Post.java
+++ src/main/java/com/pedromarzig/workshopmongo/domain/Post.java
@@ -1,14 +1,16 @@
 package com.pedromarzig.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
-import java.time.Instant;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.pedromarzig.workshopmongo.dto.AuthorDTO;
+import com.pedromarzig.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +22,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -80,6 +84,14 @@ public class Post implements Serializable{
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -91,8 +103,5 @@ public class Post implements Serializable{
 			return false;
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
-	}
-
-	
-	
+	}	
 }
